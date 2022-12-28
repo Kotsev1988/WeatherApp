6f25abe7a25014324e06082bba658d81753d598f
Al_Kotsev <115721847+Kotsev1988@users.noreply.github.com>
2022-12-28 16:57:39 +0300

    Update ViewModelCity.kt

--- app/src/main/java/com/example/weatherapp/ui/viewmodel/ViewModelCity.kt
+++ app/src/main/java/com/example/weatherapp/ui/viewmodel/ViewModelCity.kt
@@ -18,47 +18,36 @@ import kotlinx.coroutines.launch
 
 class ViewModelCity : ViewModel() {
 
-    private val repository : Repository = RepositoryImpl()
-    private val liveDataToObserve : MutableLiveData<AppStateForCity> = MutableLiveData()
+    private val repository: Repository = RepositoryImpl()
+    private val liveDataToObserve: MutableLiveData<AppStateForCity> = MutableLiveData()
 
-    fun getLiveData() : LiveData<AppStateForCity> = liveDataToObserve
+    fun getLiveData(): LiveData<AppStateForCity> = liveDataToObserve
 
     @RequiresApi(Build.VERSION_CODES.N)
-     fun getWeather(city : String) = getDataFromService(city)
+    fun getWeather(city: String) = getDataFromService(city)
 
     @RequiresApi(Build.VERSION_CODES.N)
     private fun getWeatherLoader(city: String) {
-        val loader = WeatherLoader(object : WeatherLoadListener {
+
+        repository.getWeatherFromLoader(object : WeatherLoadListener {
             override fun onSuccess(weather: Weather) {
+
                 liveDataToObserve.value =
                     AppStateForCity.Success(weather = weather)
             }
 
             override fun onFailed(throwable: Throwable) {
-                liveDataToObserve.value = AppStateForCity.Error(error = throwable.message.toString())
+
+                liveDataToObserve.value =
+                    AppStateForCity.Error(error = throwable.message.toString())
             }
 
-        }, city = city)
-        loader.loadWeather()
+        }, city = city).loadWeather()
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
-    private  fun getDataFromService(city: String) {
+    private fun getDataFromService(city: String) {
 
         getWeatherLoader(city)
-
-       /* viewModelScope.launch {
-
-
-                val weather = repository.getWeatherFromServer(city)
-                if (weather.isSuccessful) {
-                    liveDataToObserve.value = AppStateForCity.Success(weather = weather.body())
-                } else {
-                    liveDataToObserve.value = AppStateForCity.Error(error = weather.message())
-                }
-
-
-
-        }*/
     }
 }
